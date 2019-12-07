929ccee836db35c94a42083aa3ada4181bbbf99a
TamsynH <tamsyn.courtenay@gmail.com>
2019-12-07 14:57:48 +1300

    Added new print line for favourite mode of transportation

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -6,6 +6,8 @@ public class PartD {
 		System.out.println("Hi, my name is Tamsyn");
 		
 		System.out.println("My favourite colour is blue");
+		
+		System.out.println("My favourite mode of transportation is my car");
 
 	}
 
